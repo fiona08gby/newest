3528d601c7c1f80808b7b171301a6230168a1821
fiona08gby <fionagao10@gmail.com>
2023-07-14 10:24:13 -0400

    Initial Commit

---
+++ New/NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewApp.swift
+//  New
+//
+//  Created by Fiona  Gao  on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
